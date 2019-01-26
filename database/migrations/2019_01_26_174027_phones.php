86a88d24c108000ce1b60bb6e42df5f2b4b60558
petarnikolov <petarikolov97@mail.bg>
2019-01-26 21:25:01 +0200

    Database Migrations,PhonesController,basic view

--- database/migrations/2019_01_26_174027_phones.php
+++ database/migrations/2019_01_26_174027_phones.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class Phones extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,14 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email',150)->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('releaseDate');
+            $table->string('manufacturer');
+            $table->string('filename');
+
+
         });
     }
 
@@ -27,6 +31,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('phones');
+
     }
 }
